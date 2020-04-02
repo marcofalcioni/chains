fab71db9479c002f000eecc61cde59f411647b6b
Marco Falcioni <marco.falcioni@invitae.com>
2020-04-02 12:49:07 -0700

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,102 @@
+package com.invitae
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.client.*
+import io.ktor.client.features.auth.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+import io.ktor.client.features.logging.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    val client = HttpClient() {
+        install(Auth) {
+        }
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
+data class JsonSampleClass(val hello: String)
+
---
+++ test/ApplicationTest.kt
@@ -0,0 +1,32 @@
+package com.invitae
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.client.*
+import io.ktor.client.features.auth.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+import io.ktor.client.features.logging.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
